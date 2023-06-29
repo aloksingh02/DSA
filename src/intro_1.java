dab40429c3eceba9ad5b88b2100a3db480cc08a2
AbhishekVerma2001 <abhishek.verma@myanatomy.in>
2023-06-30 02:24:04 +0530

    sum of natural number

--- src/intro_1.java
+++ src/intro_1.java
@@ -1,12 +1,12 @@
 public class intro_1 {
+     static int sumOfNaturalNumber(int n)
+     {
+        return n *(n+1)/2;
+    }
+
     public static void main(String[] args) {
-        int sum = 0;
-        int num = 20;
-        for(int i = 1; i<=num; i++)
-        {
-         sum = sum + i;
-        }
-        System.out.println("sum is :" +sum);
+        int n = 10;
+        System.out.println("sum is:" +sumOfNaturalNumber(n));
     }
 }
 
