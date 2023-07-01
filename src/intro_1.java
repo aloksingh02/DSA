f514e440177e2442d9f846f8a54b7ce48a7a4398
AbhishekVerma2001 <abhishek.verma@myanatomy.in>
2023-07-02 02:53:55 +0530

    factorial

--- src/intro_1.java
+++ src/intro_1.java
@@ -1,12 +1,15 @@
 public class intro_1 {
-     static int sumOfNaturalNumber(int n)
-     {
-        return n *(n+1)/2;
-    }
+//     static int sumOfNaturalNumber(int n){
+//         return n*(n+1)/2;
+//     }
 
     public static void main(String[] args) {
-        int n = 10;
-        System.out.println("sum is:" +sumOfNaturalNumber(n));
+        int sum = 0;
+        int num = 10;
+        for(int i=0; i<=num; i++){
+            sum = sum+i;
+        }
+        System.out.println(sum);
     }
 }
 
