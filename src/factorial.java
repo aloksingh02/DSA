4b605f02ddaf8b8053ecac9b9f543d7c49ccbd78
AbhishekVerma2001 <abhishek.verma@myanatomy.in>
2023-07-02 02:48:57 +0530

    factorial

---
+++ src/factorial.java
@@ -0,0 +1,34 @@
+//ITERATIVE METHOD
+
+//public class factorial {
+//    static int factorial(int n){
+//        int res = 1;
+//        for( int i = 2; i<=n; i++){
+//            res = res *i;
+//        }
+//        return res;
+//    }
+//
+//    public static void main(String[] args) {
+//        int n = 9;
+//        System.out.println("factorial is:" +factorial(n));
+//    }
+//}
+
+
+//RECURSIVE METHOD
+public class factorial {
+    static int factorial(int n){
+        if(n==0)
+            return 1;
+        return n*factorial(n-1);
+    }
+
+    public static void main(String[] args) {
+        int n = 4;
+        System.out.println("factorial is:" +factorial(n));
+    }
+}
+
+
+
